cd7d5f0852d04ecbdc2644531d1576d08ca03ac3
Yoshibros11 <73316401+Yoshibros11@users.noreply.github.com>
2026-01-27 16:54:37 -0800

    light trolling with lines
    rise over run more like index error

--- renderer.py
+++ renderer.py
@@ -52,6 +52,10 @@ def pixel(x, y, colour):
 
 # using bresenhams line algorithm we can determine how to rasterize each pixel in a line.
 def draw_line(x0, y0, x1, y1, colour): # draw line using point intercept form
+    x0 = int(x0)
+    y0 = int(y0)
+    x1 = int(x1)
+    x0 = int(x0)
     dx = abs(x1 - x0) # dx = delta x (change in x/how far the line goes in x direction)
     dy = abs(y1 - y0) 
     sx = 1 if x0 < x1 else -1 # sx (step x) to step right (1) when x1 better than x0 else step left (-1) 
@@ -84,10 +88,19 @@ while running:
     onkey = SDL_GetKeyboardState(None)
     if onkey[SDL_SCANCODE_W]:
         print("W")
+        draw_line(int(WIDTH / 2), 0, int(WIDTH / 2), HEIGHT - 1, LIGHT_BLUE) # testing a diagonal line
+        renderStep()
     if onkey[SDL_SCANCODE_S]:
         print("S")
+        draw_line(100, 100, 300, 100, LIGHT_BLUE) # testing a diagonal line
+        draw_line(300, 100, 300, 300, LIGHT_BLUE) # testing a diagonal line
+        draw_line(300, 300, 100, 300, LIGHT_BLUE) # testing a diagonal line
+        draw_line(100, 300.67, 100, 100, LIGHT_BLUE) # testing a diagonal line
+        renderStep()
     if onkey[SDL_SCANCODE_A]:
         print("A")
+        clear()
+        renderStep()
     if onkey[SDL_SCANCODE_D]:
         print("D")
     if onkey[SDL_SCANCODE_SPACE]:
